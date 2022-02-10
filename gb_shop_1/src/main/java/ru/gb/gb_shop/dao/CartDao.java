d2bc51d4b7770b21df752e96f0ca1c5b8bd818ab
VasilevDmitry83 <VasilevDmitry83@mail.ru>
2022-02-10 12:37:07 +0300

    DZ6

---
+++ gb_shop_1/src/main/java/ru/gb/gb_shop/dao/CartDao.java
@@ -0,0 +1,7 @@
+package ru.gb.gb_shop.dao;
+
+import ru.gb.gb_shop.entity.Cart;
+
+public interface CartDao {
+    Cart save(Cart cart);
+}
---
+++ gb_shop_1/src/test/java/ru/gb/gb_shop/Les42ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.gb_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
